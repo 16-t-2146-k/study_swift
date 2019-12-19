59a9979b60603629d47145abcda17aa8562a6d5d
16-t-2146-k <16t2146k@shinshu-u.ac.jp>
2019-12-20 05:48:12 +0900

    [fix]remove git cache

---
+++ MyMap/MyMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMap
+//
+//  Created by shinta funo on 2019/12/20.
+//  Copyright © 2019 shinta funo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
